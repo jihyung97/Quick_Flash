a9127164698abb1ed6b06a738c63287dbe33e944
jihyung yoo <jihyung97@naver.com>
2025-06-23 18:53:08 +0900

    프로젝트 새로 만듬

---
+++ quickflash/src/main/java/com/quickflash/QuickflashApplication.java
@@ -0,0 +1,13 @@
+package com.quickflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickflashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickflashApplication.class, args);
+	}
+
+}
---
+++ quickflash/src/test/java/com/quickflash/QuickflashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickflash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickflashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
